ce6246c7f5502626c197f157e9b02aeb91f48de8
Duke <aboukhodr21@gmail.com>
2023-03-08 20:52:05 -0500

    returning an object

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import json5 from 'json5'
 
-export const out = { process: (src) => JSON.stringify(json5.parse(src)) }
+export const out = { process: (src) => ({ code: `export default ${JSON.stringify(json5.parse(src))}` }) }
 
 export default out
